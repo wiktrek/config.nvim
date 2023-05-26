f0395bdc2b90c4fa4becee54e4a5dfe2993ba1de
wiktrek <wiktor.janiszewski10@gmail.com>
2023-05-26 23:04:18 +0200

    lsp

--- lua/wiktrek/packer.lua
+++ lua/wiktrek/packer.lua
@@ -18,5 +18,27 @@ vim.cmd('colorscheme poimandres')
 }
 use( 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
 use('ThePrimeagen/harpoon') 
+
 use('mbbill/undotree')
- end)
+use('tpope/vim-fugitive')
+use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v2.x',
+  requires = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},             -- Required
+    {                                      -- Optional
+      'williamboman/mason.nvim',
+      run = function()
+        pcall(vim.cmd, 'MasonUpdate')
+      end,
+    },
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},     -- Required
+    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+    {'L3MON4D3/LuaSnip'},     -- Required
+  }
+}
+end)
