c5fc730729fbe6194e13c3990313d28c58d2b3ec
wiktrek <wiktor.janiszewski10@gmail.com>
2023-08-19 01:26:33 +0200

    packer

--- lua/config/packer.lua
+++ lua/config/packer.lua
@@ -3,4 +3,14 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
 use 'wbthomason/packer.nvim'
 	use 'nvim-treesitter/nvim-treesitter'
+use {
+  'glepnir/dashboard-nvim',
+  event = 'VimEnter',
+  config = function()
+    require('dashboard').setup {
+      -- config
+    }
+  end,
+  requires = {'nvim-tree/nvim-web-devicons'}
+}
 end)
--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,3 +1,3 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>p", vim.cmd.Ex)
 vim.keymap.set("n", "<C-s>", vim.cmd.w)
