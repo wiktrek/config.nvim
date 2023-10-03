14e65ca423dd02a94d84e92f44ae3e718247d2be
wiktrek <wiktor.janiszewski10@gmail.com>
2023-10-03 21:54:09 +0200

    .

--- lua/config/theme.lua
+++ lua/config/theme.lua
@@ -3,5 +3,6 @@ vim.api.nvim_create_user_command("Themes", 'Themery', {nargs = 0})
 
 -- Themery block
 -- This block will be replaced by Themery.
-vim.cmd("colorscheme Duskfox")
+vim.cmd("colorscheme dracula_blood")
+vim.g.theme_id = 9
 -- end themery block
